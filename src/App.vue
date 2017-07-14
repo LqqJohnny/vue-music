54355440b8e26c074629a68a6511314a031d63a1
lqq <lqq@gaodig.com>
2017-07-14 11:17:56 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <MHeader></MHeader>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/header/m-header.vue'
+export default {
+  name: 'app',
+components:{
+    MHeader
+}
+}
+</script>
+
+<style type="scss" scoped>
+@import "common/css/common.scss";
+
+
+
+
+
+
+</style>
---
+++ src/components/header/m-header.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+<div id="mheader" class="">
+    <div class="logo inline-block"></div>
+    <div class='inline-block'>PandaMusic</div>
+</div>
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
+
+<style type="scss">
+#mheader{
+    height:10rem;
+    width:100%;
+    background-color:rgba(0,0,0,.9);
+    text-align: center;
+    padding-top:2rem;
+    font-size: 5rem
+}
+.logo{
+    width:7rem;
+    height: 7rem;
+    background : url('../../common/images/logo.png') center 100% no-repeat;
+}
+</style>
